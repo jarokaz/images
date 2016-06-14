fdfb94d53c5d3cb6197f1d1b9552c2be5338f6fa
jakazmie <jakazmie@microsoft.com>
2016-06-14 10:30:05 -0700

    draft

--- SensorSimulator/SensorSimulator/SensorSimulator.py
+++ SensorSimulator/SensorSimulator/SensorSimulator.py
@@ -1,7 +1,37 @@
 from azure.servicebus import ServiceBusService
+import random
 
-key_name = 'RootManageSharedAccessKey' # SharedAccessKeyName from Azure portal
-key_value = '' # SharedAccessKey from Azure portal
+def getReading(sensorId,temperature,inverter_speed):                  
+    body = "{\"SensorId\" : \"" + sensorId + "\", \"Temperature\" : " + str(temperature) + ", \"InverterSpeed\" : " + str(inverter_speed) + "}"        
+    return body
+
+
+service_namespace='mtcsvcamat'
+key_name = 'All' # SharedAccessKeyName from Azure portal
+key_value = 'EUudNBNEAfkwPV1jmyh+PpeuAd3sp624MulfJGau0C0=' # SharedAccessKey from Azure portal
 sbs = ServiceBusService(service_namespace,
                         shared_access_key_name=key_name,
                         shared_access_key_value=key_value)
+
+random.seed()
+temperaturebase=20.0
+inverter_speedbase = 200.0
+count=0
+sensor='Pump1'
+
+while True:
+    count=count+1
+    if (count < 20):
+       temperature = temperaturebase 
+       inverter_speed = inverter_speedbase     
+    else:
+        temperature = temperature + round(3*random.random(),2)
+        inverter_speed = inverter_speed + round(3*random.random(),2)     
+
+    body = getReading(sensor, temperature, inverter_speed)
+    print(body)
+    sbs.send_event('amatdemo', body)
+    #if hubStatus <> 201:
+        #print("Error on " + str(datetime.datetime.now()) + " :"+str(hubStatus))
+
+
