08f671cde03d35bf89080b629c27bb9f784d12ab
jakazmie <jakazmie@microsoft.com>
2016-06-13 20:39:26 -0700

    Initial Draft

---
+++ SensorSimulator/SensorSimulator/SensorSimulator.py
@@ -0,0 +1,7 @@
+from azure.servicebus import ServiceBusService
+
+key_name = 'RootManageSharedAccessKey' # SharedAccessKeyName from Azure portal
+key_value = '' # SharedAccessKey from Azure portal
+sbs = ServiceBusService(service_namespace,
+                        shared_access_key_name=key_name,
+                        shared_access_key_value=key_value)
